a25bf052d66400d00acaf7915e27ce859a94e8f2
Andre Becker <41117972+absap@users.noreply.github.com>
2018-08-18 22:03:09 +0200

    Improve view (#41)
    * link with selection, hardening
    
    * add unlink functionality, add context menu with unlink
    
    * async backend calls

--- org.abapgit.adt.backend/src/org/abapgit/adt/backend/IRepositoryService.java
+++ org.abapgit.adt.backend/src/org/abapgit/adt/backend/IRepositoryService.java
@@ -3,8 +3,10 @@ package org.abapgit.adt.backend;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public interface IRepositoryService {
-	public IRepositories getRepositories(IProgressMonitor monitor);
+	IRepositories getRepositories(IProgressMonitor monitor);
 
-	public void cloneRepository(String url, String branch, String targetPackage, String transportRequest, String user,
+	void cloneRepository(String url, String branch, String targetPackage, String transportRequest, String user,
 			String password, IProgressMonitor monitor);
+
+	void unlinkRepository(String key, IProgressMonitor monitor);
 }
--- org.abapgit.adt.ui/src/org/abapgit/adt/ui/views/MainView.java
+++ org.abapgit.adt.ui/src/org/abapgit/adt/ui/views/MainView.java
@@ -1,5 +1,7 @@
 package org.abapgit.adt.ui.views;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.abapgit.adt.backend.IRepository;
@@ -10,13 +12,17 @@ import org.abapgit.adt.ui.wizards.AbapGitWizard;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
@@ -29,18 +35,18 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.statushandlers.StatusManager;
 
-import com.sap.adt.communication.content.ContentHandlerException;
-import com.sap.adt.communication.exceptions.CommunicationException;
-import com.sap.adt.communication.resources.ResourceException;
-import com.sap.adt.communication.resources.ResourceForbiddenException;
-import com.sap.adt.compatibility.exceptions.OutDatedClientException;
 import com.sap.adt.destinations.ui.logon.AdtLogonServiceUIFactory;
 import com.sap.adt.project.ui.util.ProjectUtil;
 import com.sap.adt.tools.core.project.AdtProjectServiceFactory;
-import com.sap.adt.tools.core.project.IAbapProject;
 
 public class MainView extends ViewPart {
 
@@ -49,6 +55,69 @@ public class MainView extends ViewPart {
 	private TableViewer viewer;
 	// private Action actionPull, actionDelete;
 	private Action actionSync, actionWizard;
+	private ISelection lastSelection;
+
+	@Override
+	public void init(IViewSite site) throws PartInitException {
+		super.init(site);
+
+		ISelection selection = getSite().getPage().getSelection();
+		if (selection instanceof ISelection) {
+			lastSelection = selection;
+		}
+	}
+
+	private ISelectionListener selectionListener = new ISelectionListener() {
+		private boolean isUpdatingSelection = false;
+
+		@Override
+		public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+			if (isUpdatingSelection)
+				return;
+
+			try {
+				isUpdatingSelection = true;
+				if (MainView.this == part)
+					return;
+
+				if (selection instanceof IStructuredSelection) {
+					IStructuredSelection structSelection = (IStructuredSelection) selection;
+					// Always take the first element - this is not intended to work with multiple
+					// selection
+					// Also, hang on to this selection for future use in case the history view is
+					// not visible
+					lastSelection = structSelection;
+
+					if (!checkIfPageIsVisible()) {
+						return;
+					}
+
+					showLastSelectedElement();
+				}
+			} finally {
+				isUpdatingSelection = false;
+			}
+		}
+	};
+
+	private IProject lastProject;
+
+	private boolean checkIfPageIsVisible() {
+		return getViewSite().getPage().isPartVisible(this);
+	}
+
+	private void showLastSelectedElement() {
+		IProject currentProject = ProjectUtil.getActiveAdtCoreProject(lastSelection, null, null, null);
+		if (currentProject != lastProject) {
+			lastProject = currentProject;
+			updateView();
+		}
+
+		// reset lastSelectedElement to null to prevent updating history view if it just
+		// gets focus
+		lastSelection = null;
+
+	}
 
 	/**
 	 * The constructor.
@@ -79,20 +148,16 @@ public class MainView extends ViewPart {
 	public void createPartControl(Composite parent) {
 		setupViewer(parent);
 
-		// Create the help context id for the viewer's control
-		// this.workbench.getHelpSystem().setHelp(this.tableViewer.getControl(),
-		// "org.abapgit.adt.ui.viewer");
-
-		// getSite().setSelectionProvider(this.viewer);
-
 		makeActions();
 		hookContextMenu();
 		contributeToActionBars();
+		// add listener for selections
+		getSite().getPage().addPostSelectionListener(selectionListener);
 	}
 
 	private void setupViewer(Composite parent) {
 		this.viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-
+		this.viewer.getControl().setEnabled(false);
 		Table table = this.viewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
@@ -155,12 +220,15 @@ public class MainView extends ViewPart {
 	}
 
 	private void hookContextMenu() {
-		MenuManager menuMgr = new MenuManager("#PopupMenu");
+		MenuManager menuMgr = new MenuManager();
 		menuMgr.setRemoveAllWhenShown(true);
 		menuMgr.addMenuListener(new IMenuListener() {
 			public void menuAboutToShow(IMenuManager manager) {
-				if (viewer.getStructuredSelection().size() == 0) {
-					return;
+				if (viewer.getStructuredSelection().size() == 1) {
+					Object firstElement = viewer.getStructuredSelection().getFirstElement();
+					if (firstElement instanceof IRepository) {
+						manager.add(new UnlinkAction(lastProject, (IRepository) firstElement));
+					}
 				}
 				// MainView.this.fillContextMenu(manager);
 			}
@@ -202,7 +270,7 @@ public class MainView extends ViewPart {
 
 		this.actionSync = new Action() {
 			public void run() {
-				fetchRepos();
+				updateView();
 				// viewer.setInput(Repository.list());
 				// viewer.setInput(getRepoList());
 				// viewer.setInput(getRepositories());
@@ -212,7 +280,7 @@ public class MainView extends ViewPart {
 		this.actionSync.setToolTipText("Syncronize");
 		this.actionSync
 				.setImageDescriptor(AbapGitUIPlugin.getDefault().getImageDescriptor(ISharedImages.IMG_ELCL_SYNCED));
-
+		this.actionSync.setEnabled(false);
 		// this.actionCreate = new Action() {
 		// public void run() {
 		// CreateDialog dialog = new CreateDialog(viewer.getControl().getShell());
@@ -241,10 +309,10 @@ public class MainView extends ViewPart {
 				WizardDialog wizardDialog = new WizardDialog(viewer.getControl().getShell(), new AbapGitWizard());
 
 				if (wizardDialog.open() == Window.OK) {
-					fetchRepos();
+					updateView();
 				} else {
 					// System.out.println("Cancel pressed");
-					fetchRepos();
+					updateView();
 				}
 			}
 		};
@@ -252,66 +320,113 @@ public class MainView extends ViewPart {
 		this.actionWizard.setToolTipText("Clone Repository");
 		this.actionWizard
 				.setImageDescriptor(AbapGitUIPlugin.getDefault().getImageDescriptor(ISharedImages.IMG_OBJ_ADD));
+		this.actionWizard.setEnabled(false);
 	}
 
-	public List<IRepository> getRepositories() {
-		IProgressMonitor monitor = null;
-
-		// example
-		IProject project = ProjectUtil.getActiveAdtCoreProject(null, null, null, IAbapProject.ABAP_PROJECT_NATURE);
-
-		if (project == null) {
+	private List<IRepository> getRepositories() {
+		if (lastProject == null) {
 			return null;
 		}
 
-		IStatus logonStatus = AdtLogonServiceUIFactory.createLogonServiceUI().ensureLoggedOn(project);
+		IStatus logonStatus = AdtLogonServiceUIFactory.createLogonServiceUI().ensureLoggedOn(lastProject);
 		if (!logonStatus.isOK()) {
 			return null;
 		}
 
-		String destinationId = AdtProjectServiceFactory.createProjectService().getDestinationId(project);
-
-		IRepositoryService repoService = RepositoryServiceFactory.createRepositoryService(destinationId, monitor);
-		List<IRepository> repos = null;
+		String destinationId = getDestination(lastProject);
+		List<IRepository> repos = new LinkedList<>();
 		try {
-			repos = repoService.getRepositories(monitor).getRepositories();
-		
-		} catch (OutDatedClientException e) {
-			// bring up popup about too old client, ...
-			System.out.println(e.getMessage());
-			return null;
-		} catch (ResourceForbiddenException e) { // 403
-			String subtype = e.getErrorInfo().getExceptionData().getSubtype();
-			System.out.println(subtype);
-		} catch (ResourceException e) {
-			System.out.println(e.getMessage());
-		} catch (CommunicationException e) {
-			// something else went wrong (network layer)
-			System.out.println(e.getMessage());
+			PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
+
+				@Override
+				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+					monitor.beginTask("Fetching repositories", IProgressMonitor.UNKNOWN);
+					IRepositoryService repoService = RepositoryServiceFactory.createRepositoryService(destinationId,
+							monitor);
+					if (repoService == null) {
+						return;
+					}
+					repos.addAll(repoService.getRepositories(monitor).getRepositories());
+				}
+			});
+
+		} catch (InvocationTargetException e) {
+			StatusManager.getManager().handle(new Status(IStatus.ERROR, AbapGitUIPlugin.PLUGIN_ID,
+					"Error fetching repositories", e.getTargetException()), StatusManager.SHOW);
+		} catch (InterruptedException e) {
 		}
 		return repos;
-
 	}
 
-	private void fetchRepos() {
-		try {
-			List<IRepository> repos = getRepositories();
-			
-			if (repos != null) {
-				this.viewer.setInput(repos);
-			}
-		} catch (ContentHandlerException ex) {
-			System.out.println(ex.getMessage());
+	private static String getDestination(IProject project) {
+		return AdtProjectServiceFactory.createProjectService().getDestinationId(project);
+	}
+
+	private void updateView() {
+		List<IRepository> repos = getRepositories();
+
+		if (repos != null && !repos.isEmpty()) {
+			this.viewer.getControl().setEnabled(true);
+			this.actionSync.setEnabled(true);
+			this.actionWizard.setEnabled(true);
+			this.viewer.setInput(repos);
+		} else {
+			this.viewer.getControl().setEnabled(false);
+			this.actionSync.setEnabled(false);
+			this.actionWizard.setEnabled(false);
+			this.viewer.setInput(null);
 		}
 	}
-	
+
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	@Override
 	public void setFocus() {
 		this.viewer.getControl().setFocus();
-		this.fetchRepos();
+
+		if (lastSelection != null) {
+			showLastSelectedElement();
+		}
+	}
+
+	@Override
+	public void dispose() {
+		getSite().getPage().removePostSelectionListener(selectionListener);
+
+		super.dispose();
+	}
+
+	private class UnlinkAction extends Action {
+
+		private final IProject project;
+		private final IRepository repository;
+
+		public UnlinkAction(IProject project, IRepository repository) {
+			this.project = project;
+			this.repository = repository;
+			setText("Unlink");
+		}
+
+		@Override
+		public void run() {
+			try {
+				PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
+
+					@Override
+					public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+						RepositoryServiceFactory.createRepositoryService(getDestination(project), monitor)
+								.unlinkRepository(repository.getKey(), monitor);
+					}
+				});
+				updateView();
+			} catch (InvocationTargetException e) {
+				StatusManager.getManager().handle(new Status(IStatus.ERROR, AbapGitUIPlugin.PLUGIN_ID,
+						"Error unlinking repository", e.getTargetException()), StatusManager.SHOW);
+			} catch (InterruptedException e) {
+			}
+		}
+
 	}
 
 }
