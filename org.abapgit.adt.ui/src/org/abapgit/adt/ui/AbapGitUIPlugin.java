6b34b7604aa0fc00632e2356c035bbae5a9a33df
i323324 <abin.baby@sap.com>
2019-08-12 11:03:43 +0530

    Extend the plug-in activator class from AbstractUIPlugin
    Extend the plug-in activator class "AbapGitUIPlugin" with the
    "AbstractUIPlugin" base class for more control over the plugin instance.

--- org.abapgit.adt.ui/src/org/abapgit/adt/ui/AbapGitUIPlugin.java
+++ org.abapgit.adt.ui/src/org/abapgit/adt/ui/AbapGitUIPlugin.java
@@ -1,7 +1,33 @@
 package org.abapgit.adt.ui;
 
-public final class AbapGitUIPlugin {
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public final class AbapGitUIPlugin extends AbstractUIPlugin {
+
+	private static AbapGitUIPlugin plugin;
 
 	public static final String PLUGIN_ID = "org.abapgit.adt.ui"; //$NON-NLS-1$
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static AbapGitUIPlugin getDefault() {
+		return plugin;
+	}
 
 }
