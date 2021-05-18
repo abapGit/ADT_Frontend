8b71308ac20a3aabee3a8d1cb2d2a8627b7e7a48
Abin Baby <51697829+abinbaby-sap@users.noreply.github.com>
2021-05-18 13:05:14 +0530

    Fix failing tests because of PR #205 (#208)
    * Fix failing tests because of PR #205
    
    https://github.com/abapGit/ADT_Frontend/pull/205
    
    * Increase easymock dependency to 4.2

--- test/org.abapgit.adt.ui.test/src/org/abapgit/adt/ui/internal/util/TestsUnitAbapGitService.java
+++ test/org.abapgit.adt.ui.test/src/org/abapgit/adt/ui/internal/util/TestsUnitAbapGitService.java
@@ -6,46 +6,46 @@ import org.abapgit.adt.backend.model.abapgitrepositories.impl.Abapgitrepositorie
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.sap.adt.tools.core.model.adtcore.IAdtCoreFactory;
 import com.sap.adt.tools.core.model.atom.IAtomFactory;
 import com.sap.adt.tools.core.model.atom.IAtomLink;
 
-
 public class TestsUnitAbapGitService {
-	
 	@Test
 	public void isSelectivePullSupported() {
-
-	 //Prepare test data.	
+		// Prepare test data.
 		// Create Atom Link for modified objects
 		IAtomLink modifiedObjects = IAtomFactory.eINSTANCE.createAtomLink();
 		modifiedObjects.setType("modifiedobjects_link");
 		modifiedObjects.setRel(IRepositoryService.RELATION_MODIFIED_OBJECTS);
-		
-		//repository with atom link returned from backend for modified objects, supports selective pull
+
+		// repository with atom link returned from backend for modified objects,
+		// supports selective pull
 		IRepository repositoryWithModifiedObjectsLink = AbapgitrepositoriesFactoryImpl.eINSTANCE.createRepository();
-		repositoryWithModifiedObjectsLink.setUrl("https://github.com/url_one");		
+		repositoryWithModifiedObjectsLink.setUrl("https://github.com/url_one");
 		repositoryWithModifiedObjectsLink.setPackage("dummy_package_one");
 		repositoryWithModifiedObjectsLink.setCreatedEmail("dummy_user@email.com");
 		repositoryWithModifiedObjectsLink.setBranchName("master");
 		repositoryWithModifiedObjectsLink.setDeserializedAt("20200322180503");
-		repositoryWithModifiedObjectsLink.setStatusText("dummy_status");		
+		repositoryWithModifiedObjectsLink.setStatusText("dummy_status");
 		repositoryWithModifiedObjectsLink.getLinks().add(modifiedObjects);
 
-		//repository with no atom link for modified objects, older backend versions than 2105 don't support selective pull
+		// repository with no atom link for modified objects, older backend versions
+		// than 2105 don't support selective pull
 		IRepository repositoryWithOutModifiedObjectsLink = AbapgitrepositoriesFactoryImpl.eINSTANCE.createRepository();
-		repositoryWithOutModifiedObjectsLink.setUrl("https://github.com/url_one");		
+		repositoryWithOutModifiedObjectsLink.setUrl("https://github.com/url_one");
 		repositoryWithOutModifiedObjectsLink.setPackage("dummy_package_one");
 		repositoryWithOutModifiedObjectsLink.setCreatedEmail("dummy_user@email.com");
 		repositoryWithOutModifiedObjectsLink.setBranchName("master");
 		repositoryWithOutModifiedObjectsLink.setDeserializedAt("20200322180503");
-		repositoryWithOutModifiedObjectsLink.setStatusText("dummy_status");		
+		repositoryWithOutModifiedObjectsLink.setStatusText("dummy_status");
 
-	 // Execute code under test.
+		// Execute code under test.
 		IAbapGitService abapGitService = AbapGitUIServiceFactory.createAbapGitService();
-		Assert.assertEquals(true, abapGitService.isSelectivePullSupported(repositoryWithModifiedObjectsLink));
+		
+//      Assert statement commented as selective pull is disabled temporarily
+//      TODO: enable the test back once the feature is enabled back again
+//		Assert.assertEquals(true, abapGitService.isSelectivePullSupported(repositoryWithModifiedObjectsLink));
 		
 		Assert.assertEquals(false, abapGitService.isSelectivePullSupported(repositoryWithOutModifiedObjectsLink));
 	}
-	
 }
